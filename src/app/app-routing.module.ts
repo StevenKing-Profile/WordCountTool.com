b9dbbb2faf494f555ee329d32310ac9b05974618
Steven King <nfstevenking@gmail.com>
2019-03-25 19:26:00 -0400

    Revert "Router changes to nav bar and various css fixes"
    This reverts commit ea9cca238686e1dc739d635534c4c7e18f576232.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { MlaComponent } from './mla/mla.component';
 import { CountComponent } from './Count/count.component';
 
 const appRoutes: Routes = [
-    { path: '', component: CountComponent },
+    { path: 'count', component: CountComponent },
     { path: 'mla', component: MlaComponent },
-    // { path: '**', redirectTo: '/' }
+    { path: '**', redirectTo: '/' }
   ]
 
   @NgModule({
